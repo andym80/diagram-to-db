6fdfaa37dac6a6c685b70f43f0aedc4b8649e7f2
Andy Menutti <andymenutti@gmail.com>
2022-10-25 18:54:02 -0300

    add all tables

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE clinic;
+CREATE TABLE patients (id INT PRIMARY KEY, name varchar(50), date_of_bith date);
+CREATE TABLE medical_histories (id INT PRIMARY KEY,admitted_at TIMESTAMP, patient_id INT,status VAR
+CHAR(20));
+CREATE TABLE treatments (id INT PRIMARY KEY,type VARCHAR(25),name VARCHAR(50));
+CREATE TABLE invoice_items (id INT PRIMARY KEY, unit_price DECIMAL,quantity INT,total_price DECIMAL,
+invoice_id INT,treatment_id INT);
+CREATE TABLE invoices (id INT PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP,payed_at TI
+MESTAMP,medical_history_id INT);
+
+--
+ALTER TABLE medical_histories ADD CONSTRAINT pk_patient_id FOREIGN KEY(patient_id) REFERENCES  patients(id);
+ALTER TABLE invoices ADD CONSTRAINT pk_medical_history_id FOREIGN KEY(medical_history_id) REFERENCES  medical_histories(id);
+ALTER TABLE invoice_items ADD CONSTRAINT pk_invoice_id FOREIGN KEY(invoice_id) REFERENCES  invoices(id);
+ALTER TABLE treatments ADD COLUMN medical_histories_id INT;
+ALTER TABLE treatments ADD CONSTRAINT pk_medical_histories_id FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id);
+ALTER TABLE treatments ADD COLUMN invoice_items_id INT;
+ALTER TABLE treatments ADD CONSTRAINT pk_invoice_items_id FOREIGN KEY (invoice_items_id) REFERENCES invoice_items(id);
+
+
+
+
+
